8e15452f9d850e819f9f0b1a158f7512034090b5
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-04-24 16:38:15 +0800

    chore: detail, results and vote views for question.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
 
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
+
 def index(request):
     return HttpResponse("Hello visitor. You're at the poll index")

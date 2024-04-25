9a2d20e000ee2d2165860d1a1927e4d5984b3f77
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-04-25 14:33:03 +0800

    chore: use generic views.

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,11 @@
+from typing import Any
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, HttpResponseRedirect
 from polls.models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", context={"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", context={"question": question})
+from django.views import generic
 
 
 def vote(request, question_id):
@@ -35,7 +28,20 @@ def vote(request, question_id):
         return HttpResponseRedirect((reverse("polls:results", args=(question.id,))))
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
